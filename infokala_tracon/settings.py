813ffb18b50d4fe854b24a14c6a472d493c628cf
Santtu Pajukanta <santtu@pajukanta.fi>
2019-07-21 13:54:17 +0300

    allow jv to access

--- infokala_tracon/settings.py
+++ infokala_tracon/settings.py
@@ -104,7 +104,7 @@ INFOKALA_ACCESS_GROUP_TEMPLATES = [
     '{infokala_installation_slug}-{event_slug}-users',
     '{kompassi_installation_slug}-{event_slug}-labour-conitea',
     '{kompassi_installation_slug}-{event_slug}-labour-info',
-    # '{kompassi_installation_slug}-{event_slug}-labour-jarjestyksenvalvoja',
+    '{kompassi_installation_slug}-{event_slug}-labour-jarjestyksenvalvoja',
     # '{kompassi_installation_slug}-{event_slug}-labour-jv',
 ]
 INFOKALA_DEFAULT_EVENT = env('INFOKALA_DEFAULT_EVENT', default='tracon2017')
